d147b50b0a1c887f5f9551ef43fae3ebdbce1c32
mohamadrezabjr <mohamadrezabjr@gmail.com>
2025-01-02 04:01:41 +0330

    making main page and showing products

--- Shop/admin.py
+++ Shop/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from Shop.models import *
 
+admin.site.register(Product)
 # Register your models here.
--- Shop/views.py
+++ Shop/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from tempfile import template
 
-# Create your views here.
+from django.shortcuts import render
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
+from django.shortcuts import render , redirect
+from .models import *
+from django.http import HttpResponse
+from django.template import loader
+from django.views.decorators.csrf import csrf_exempt
+from datetime import datetime
+
+def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
+
+def product(request):
+
+    products = Product.objects.all()
+    context = {'products': products}
+    return render (request, 'products.html', context=context)
+
