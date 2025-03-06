01dad034e78ba062d786e292f8ce112b4ac9b5d0
mohamadreza <mohamadrezabjr@gmail.com>
2025-03-07 02:46:42 +0330

    Category image

--- Shop/models.py
+++ Shop/models.py
@@ -17,7 +17,6 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
